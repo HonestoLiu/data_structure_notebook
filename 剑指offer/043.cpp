073b2e5465c1ef3fe6a431793c28b236b65d85f8
Holden Lyo <21735039@zju.edu.cn>
2019-05-14 13:13:32 +0800

    和为S的连续正数序列

--- 剑指offer/043.cpp
+++ 剑指offer/043.cpp
@@ -1,20 +1,41 @@
 /*
-�ҳ����к�ΪS��������������, 100=9-16,18-22
+找出所有和为S的连续正数序列, 100=9-16,18-22
 */
 
 class Solution {
 public:
+	//自己，设序列为{x+1,x+2,...,x+len}，求解满足条件的整数x>=0
+	//从长度len=2验证 
     vector<vector<int> > FindContinuousSequence(int sum) {
+        vector<vector<int> > res;
+        vector<int> tmp;
+        int add=3,len=2;
+        while(add<=sum){
+            if((sum-add)%len==0){
+                tmp.clear();
+                int beg=(sum-add)/len;
+                for(int j=1;j<=len;j++)
+                    tmp.push_back(beg+j);
+                res.insert(res.begin(),tmp);
+            }
+            len++;
+            add+=len;
+        }
+        return res;
+    }
+    
+    //牛客网 --滑动指针的做法
+	vector<vector<int> > FindContinuousSequence(int sum) {
 		vector<vector<int> > res;
 		vector<int> tmp;
 		int plow=1,phigh=2,add=0;
 		while(plow<phigh){
-			add = (plow+phigh)*(phigh-plow+1)/2;	//��͹�ʽ 
+			add = (plow+phigh)(phigh-plow+1)/2;	//求和公式 
 			if(add==sum){
 				tmp.clear();
 				for(int i=plow;i<=phigh;i++)
                     tmp.push_back(i);
-                res.push_back(tmp);
+                res.push_back[tmp];
                 plow++;
 			}
 			else if(add<sum)
@@ -23,5 +44,6 @@ public:
 				plow++;
 		}
 		return res;
-    }
+	}
+	 
 };
